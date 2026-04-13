9894f64bf069d20e7680457ec00c9878e6606c12
Prakhar Shukla <prakharshukla357@gmail.com>
2026-04-13 17:18:42 +0530

    Added trained model file

---
+++ src/models/cnn_model.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(128 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 32x16x16
+        x = self.pool(F.relu(self.conv2(x)))  # 64x8x8
+        x = self.pool(F.relu(self.conv3(x)))  # 128x4x4
+
+        x = x.view(-1, 128 * 4 * 4)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
